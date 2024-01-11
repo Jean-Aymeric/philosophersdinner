efd39e0315bf24bf38d5441f608a4fcffcaf0451
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2024-01-11 11:37:53 +0100

    add time between taking the two forks

--- src/main/java/com/jad/philosophersdinner/Philosopher.java
+++ src/main/java/com/jad/philosophersdinner/Philosopher.java
@@ -2,9 +2,10 @@ package com.jad.philosophersdinner;
 
 public class Philosopher implements Runnable {
 
-  private static final long EATING_TIME = 100;
-  private static final long MAX_HUNGRY_TIME = 1000;
-  private static final long MAX_THINKING_TIME = 900;
+  private static final long EATING_TIME = 400;
+  private static final long MAX_HUNGRY_TIME = 10000;
+  private static final long MAX_THINKING_TIME = 100;
+  private static final long TIME_BETWEEN_TAKING_FORKS = 70;
   private final Fork leftFork;
   private final Fork rightFork;
   private final String name;
@@ -37,12 +38,13 @@ public class Philosopher implements Runnable {
     Thread.sleep((long) (Math.random() * Philosopher.MAX_THINKING_TIME));
   }
 
-  private void takeForks() {
+  private void takeForks() throws InterruptedException {
     this.state = PhilosopherState.HUNGRY;
     System.out.println(this.name + " is hungry");
 
     this.rightFork.lock();
     System.out.println(this.name + " took right fork");
+    Thread.sleep(Philosopher.TIME_BETWEEN_TAKING_FORKS);
     this.leftFork.lock();
     System.out.println(this.name + " took left fork");
 
